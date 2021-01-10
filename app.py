11013e1a58d3bd232e46b214c8f429a23adc8385
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-01-10 03:05:50 +0000

    Created signup query and editted signup template

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'image_repo'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-app.config['IMAGE_UPLOADS'] = '/home/tony-medici/Projects/POS/static/images/logo'
-app.config['ALLOWED_IMAGE_EXTENSION'] = ['PNG', 'JPG', 'JPEG', 'GIF']
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+# app.config['IMAGE_UPLOADS'] = '/home/tony-medici/Projects/POS/static/images/logo'
+# app.config['ALLOWED_IMAGE_EXTENSION'] = ['PNG', 'JPG', 'JPEG', 'GIF']
+# app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 
 
 # init MYSQL
@@ -30,8 +30,36 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    # Get Signup Template Form Fields
+    if request.method == 'POST':
+        fullname =request.form['fullname']
+        username = request.form['username']
+        email = request.form['email']
+        encrpt = request.form['password']
+        password = sha256_crypt.encrypt(str(encrpt)) # Encrypt user password
+
+        # Create Cursor for Quesries o the database
+        cur = mysql.connection.cursor()
+
+        # Execute Query to check if user already exists else insert into databse
+        result = cur.execute("SELECT email FROM users WHERE email=%s", [email])
+        if result > 0:
+                flash("Sorry, Account ALready Exits", "danger")
+                return redirect(url_for('login'))
+        else:
+            cur.execute("INSERT users (full_name, username, email, password, create_date, create_time) VALUES(%s, %s, %s, %s, Now(), Now())", (fullname , username, email, password))
+
+        # Commit To Database
+        mysql.connection.commit()
+
+        # CLose Connection
+        cur.close()
+            
+        flash('You are successfully registered', 'success')
+        return redirect(url_for('login'))
+
     return render_template('signup.html')
 
 
@@ -45,18 +73,18 @@ def logout():
     pass
 
 
-@app.route('user/<int:user_id>/<int:image_id>/upload_image')
+@app.route('/user/<int:user_id>/<int:image_id>/upload_image')
 def imageUpload(user_id, image_id):
     pass
 
 
-@app.route('user/<int:user_id>/<int:image_id>/edit_image')
+@app.route('/user/<int:user_id>/<int:image_id>/edit_image')
 def editImage(user_id, image_id):
     pass
 
 
-@app.route('user/<int:user_id>/<int:image_id>/delete_image')
-def editImage(user_id, image_id):
+@app.route('/user/<int:user_id>/<int:image_id>/delete_image')
+def deleteImage(user_id, image_id):
     pass
 
 @app.route('/user/<int:user_id>/profile')
@@ -64,7 +92,7 @@ def userProfile(user_id):
     pass
 
 
-@app.route('/search'):
+@app.route('/search')
 def search():
     pass
 
